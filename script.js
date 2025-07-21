5c26fc79a1a5f284b957baa2eba82c474ec6c4db
ecosdelalma <Lmcmorales86@gmail.com>
2025-07-21 08:38:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("be-ecos-button").addEventListener("click", function () {
+  document.querySelector(".overlay").classList.add("hidden");
+  document.getElementById("signup-form").classList.remove("hidden");
+});
